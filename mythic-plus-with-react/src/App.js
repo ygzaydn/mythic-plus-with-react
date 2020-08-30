99c19a8e69264425d01d8b3b247a207ee4215749
ygzaydn <ygzaydns@github.com>
2020-08-30 19:43:15 +0300

    initalization

---
+++ mythic-plus-with-react/src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      result: [],
+    }
+  }
+  componentDidMount(){
+    axios.get('https://raider.io/api/v1/mythic-plus/affixes?region=eu&locale=en')
+    .then(res => {
+      const affixes = res.data.affix_details;
+      this.setState({ result: affixes})})
+  }
+  
+  render(){
+    const { result } = this.state;
+    console.log(result);
+    return (
+      <div>
+       <h1>M+ Affixes for this week!</h1>,
+      {result.map(el => {
+        return (
+          <Affix 
+            name={el.name}
+            description={el.description}
+            url={el.wowhead_url}
+            id={el.id}
+          />
+        )
+        
+      })}
+      </div>
+
+    )
+  }
+}
+
+function Affix({name, description, url, id}){
+  return (
+    <div key={id}>
+      <h2>Name of the affix:{name}</h2>
+      <h2>Description of the affix:{description}</h2>
+      <h2>Wowhead URL:{url}</h2>
+    </div>
+  )
+}
+
+
+export default App;
